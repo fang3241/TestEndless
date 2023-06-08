97a561bb639595c838725ce61d7b4003de8e0336
fang3241 <mikhsanmf@gmail.com>
2023-06-08 13:59:13 +0700

    FirstBuild
    FB

---
+++ testEndless/Assets/Script/Collectable.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : Collidable
+{
+    
+    protected override void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.name);
+        OnCollect();
+    }
+
+    protected virtual void OnCollect()
+    {
+        Debug.Log("Collected");
+    }
+}
---
+++ testEndless/Assets/Script/ObstacleSpawner.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : SpawnerCycle
+{
+    //public float obsSpawnInterval;
+
+    //protected override void Update()
+    //{
+    //    spawnInterval = obsSpawnInterval;
+    //    base.Update();
+    //}
+
+}
